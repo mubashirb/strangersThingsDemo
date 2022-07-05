a47292a9ac7210646e899c0c985c16f30527cb87
mubashirb <mubashir.bukhari7@gmail.com>
2022-07-05 16:40:36 -0400

    Activities-Base populating, need 2 wrk on form nxt

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,85 +1,121 @@
 import React, { useEffect, useState } from 'react';
-export default function Activities({ loggedIn, token }) {
+import reactdomclient  from "react-dom/client";
+import { BrowserRouter, useNavigate, Routes, Route, Link } from "react-router-dom";
 
-    const [activities, setActivities ] = useState([]);
-    const [activityName, setActivityName] = useState('');
-    const [activityDescription, setActivityDescription] = useState('')
+export default function Activities({loggedIn, token}){
+    const [activities, setActivities] = useState([]);
 
-    useEffect(() => {
+    useEffect(()=>{
 
-        async function getActivities() {
-            try {
+        async function getActivities(){
+            try{
                 const response = await fetch('http://fitnesstrac-kr.herokuapp.com/api/activities', {
                     headers: {
-                        'Content-Type': 'application/json',
-                      },
-                })
-
-                let data = await response.json()
-
-                setActivities(data.data.description)
-
-            } catch (err) {
-                console.log(err)
-
+                      'Content-Type': 'application/json',
+                    },
+                  })
+                  let data = await response.json()
+                  
+                  
+                  setActivities(data)
             }
+        catch(err){
+            console.log(error)
         }
-        getActivities()
-    }, [token])
+    }
+    getActivities()
+    },[])
 
-
-
-    // //CREATE ACTIVITY 
-    async function createActivity(activityid, token) {
-        try {
-            const response = await fetch(`http://fitnesstrac-kr.herokuapp.com/api/activities/${activityid}`, {
-            //     method: "POST",
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //         'Authorization': `Bearer ${token}`
-            //     },
-            //     body: JSON.stringify({
-            //         //message?
-            //         message: {
-            //             content: description
-            //         }
-            //     })
-            // })
-            method: "POST",
-            body: JSON.stringify({
-              name: activityName,
-              description: activityDescription
+    return (
+        <>
+        <h1> Activities</h1>
+        {
+            activities.map(activity => {
+                return <li key = {activity.id}>
+                    <div>{activity.id}</div>
+                    <div>{activity.name}</div>
+                    <div>{activity.description}</div>
+                    </li>
             })
-          })
-            let data = await response.json()
-            console.log(data)
-        } catch (err) {
-            console.log(err)
         }
-    }
-    {
-        return (
-            <>
-                {   loggedIn ?
-                    activities.map((activity) => {
-                        return (
-                            <div key={activity._id}>
-                                <div className="card">
-                                    <div><h4>Name{activity.name}</h4></div>
-                                    <div>Description:{activity.description}</div>
-
-                                    <label></label>
-                                    <input type="text" placeholder="Create Activity" onChange={(event) => { createActivity(event.target.value) }} ></input>
-                                    <br />
-                                    <button type="submit" className="btCreate"
-                                        onClick={() => createActivity(activity._id)}>Create Activity</button>
-                                </div>
-                                <br />
-                            </div>
-                        )
-                    }) : null
-                }
-            </>
-        )
-    }
+        </>
+    )
 }
+
+
+
+// export default function Activities({ loggedIn, token }) {
+
+//     const [activities, setActivities ] = useState([]);
+//     const [activityName, setActivityName] = useState('');
+//     const [activityDescription, setActivityDescription] = useState('')
+
+//     useEffect(() => {
+
+//         async function getActivities() {
+//             try {
+//                 const response = await fetch('http://fitnesstrac-kr.herokuapp.com/api/activities', {
+//                     headers: {
+//                         'Content-Type': 'application/json',
+//                         'Authorization': `${token}`
+//                       },
+//                 })
+
+//                 let data = await response.json()
+//                 console.log(data)
+
+//                 // setActivities(data.data.id)
+
+//             } catch (err) {
+//                 console.log(err)
+
+//             }
+//         }
+//         getActivities()
+//     }, [token])
+//     async function createActivity(activityid, token) {
+//         try {
+//             const response = await fetch(`http://fitnesstrac-kr.herokuapp.com/api/activities/`, {
+         
+//             method: "POST",
+//             body: JSON.stringify({
+//                 activity: {
+//               name: activityName,
+//               description: activityDescription
+//                 }
+//             })
+//           })
+//             let data = await response.json()
+//             setActivityName(data.activity.name)
+//             setActivityDescription(data.acitivity.description)
+//             console.log(data)
+//         } catch (err) {
+//             console.log(err)
+//         }
+//     }
+//     {
+//         return (
+//             <>
+//                 {   loggedIn ?
+//                     activities.map((activity) => {
+//                         return (
+//                             <div key={activity.id}>
+//                                 <div className="card">
+//                                     <div><h4>Name{activity.name}</h4></div>
+//                                     <div>Description:{activity.description}</div>
+
+//                                     <label></label>
+//                                     <input type="text" placeholder="Create Activity" onChange={(event) => { createActivity(event.target.value) }} ></input>
+//                                     <br />
+//                                     <button type="submit" className="btCreate"
+//                                         onClick={() => createActivity(activity.id)}>Create Activity</button>
+//                                 </div>
+//                                 <br />
+//                             </div>
+//                         )
+//                     }) : null
+//                 }
+//             </>
+//         )
+//     }
+// }
